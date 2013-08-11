1f9fc14f29717ec914803ffe793b6e2eb1c3d4a9
Matt Palmer <mpalmer@hezmatt.org>
2013-08-11 17:33:11 +1000

    Shut up about not being able to open databases

--- src/nds.c
+++ src/nds.c
@@ -184,9 +184,11 @@ success:
         goto err_cleanup;
     }
     
-    if ((rv = mdb_dbi_open(ndsdb->txn, ndsdb->db_name, MDB_CREATE, &(ndsdb->dbi)))) {
-        redisLog(REDIS_WARNING, "Failed to open freezer DBi for DB %i: %s", db->id, mdb_strerror(rv));
-        goto err_cleanup;
+    if ((rv = mdb_dbi_open(ndsdb->txn, ndsdb->db_name, writer ? MDB_CREATE : 0, &(ndsdb->dbi)))) {
+        if (writer || rv != MDB_NOTFOUND) {
+            redisLog(REDIS_WARNING, "Failed to open freezer DBi for DB %i: %s", db->id, mdb_strerror(rv));
+            goto err_cleanup;
+        }
     }
 
     /* Epic win! */
